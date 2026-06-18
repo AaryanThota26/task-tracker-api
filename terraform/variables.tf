5edeb79f929244dab385b2fe432f280323b185df
Aaryan Thota <aaryanthota2@gmail.com>
2026-06-18 18:41:25 +0530

    Deploy task tracker on GKE with Terraform and custom domain

---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "project_id" {
+  default = "task-tracker-499005"
+}
+
+variable "region" {
+  default = "us-central1"
+}
+
+variable "cluster_name" {
+  default = "task-tracker-cluster"
+}
